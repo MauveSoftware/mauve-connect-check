8b8b20508da16d458c62d2475835e35643045ce1
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-06-17 15:44:19 +0200

    refactoring: add enum for exit codes

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,20 @@ use std::process::exit;
 use crate::check::check_mauve_dns;
 use crate::output::print_check_result;
 
+enum ExitCode {
+    MissingArgument = 1,
+    CheckError = 2,
+    CheckPassed = 0,
+    CheckFailed = -1,
+}
+
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 2 {
         eprintln!("Usage: {} <domain>", args[0]);
-        exit(1);
+        exit(ExitCode::MissingArgument as i32);
     }
     let domain = &args[1];
 
@@ -22,14 +29,14 @@ async fn main() {
         Ok(result) => {
             print_check_result(domain, &result);
             if result.success {
-                exit(0);
+                exit(ExitCode::CheckPassed as i32);
             } else {
-                exit(-1);
+                exit(ExitCode::CheckFailed as i32);
             }
         }
         Err(e) => {
             eprintln!("Error: {}", e);
-            exit(2);
+            exit(ExitCode::CheckError as i32);
         }
     }
 }
