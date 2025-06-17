8078489c8d8fe6fcabb843bff3dff706d9624cf9
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-06-17 15:38:23 +0200

    refactor: const for base URL to improve readability

--- src/check.rs
+++ src/check.rs
@@ -2,11 +2,10 @@ use std::error::Error;
 
 use crate::check_result::CheckResult;
 
+const BASE_URL: &str = "https://domain-manager.infra.mauve.cloud/api/dns/check/";
+
 pub async fn check_mauve_dns(domain: &String) -> Result<CheckResult, Box<dyn Error>> {
-    let url = format!(
-        "https://domain-manager.infra.mauve.cloud/api/dns/check/{}",
-        domain
-    );
+    let url = format!("{}{}", BASE_URL, domain);
     let resp = reqwest::get(url).await?;
     if resp.status() != reqwest::StatusCode::OK {
         let mut err_msg = format!("HTTP error: {}", resp.status());
